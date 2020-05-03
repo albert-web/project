d911007517871f504de8c039891807c96339a8ea
Ubuntu <ubuntu@ip-172-31-31-199.us-east-2.compute.internal>
2020-05-03 09:14:39 +0000

    modified app.js to say Albert Mensah do not edit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        DO NOT Edit <code>src/App.js</code> and save to reload.
+      Albert Mensah DO NOT Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
